0cd7e7f0ecb41e9accc8e8cd52fee34007a5d0fc
Tiago Monteiro <monteiro.tg@gmail.com>
2017-09-27 21:31:00 -0300

    'testes'

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-//app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use('/', routes);
