dea841a61c3e4595df514b6a59bdcd21071f3592
Tiago Monteiro <monteiro.tg@gmail.com>
2017-09-27 21:55:14 -0300

    'testes-db'

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-app.get('/db', function (request, response) {
+router.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
