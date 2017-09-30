a31a4d3deb675192390ca06ce5d16b2428dabdbe
Tiago Monteiro <monteiro.tg@gmail.com>
2017-09-30 18:19:21 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-/*var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk(process.env.OPENSHIFT_MONGODB_DB_URL + 'shaolin');*/
+const routes = require('./routes/index');
 
-var routes = require('./routes/index');
-
-var app = express();
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-//app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 /*app.use(function(req,res,next){
 	req.db = db;
@@ -28,18 +32,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
 
-
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-/*app.get('/', function(request, response) {
-  response.render('pages/index');
-});*/
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+module.exports = app;
