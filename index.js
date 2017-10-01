f9a79456294fdc7d0dd79636bb74a5b0a98c36f5
Tiago Monteiro <monteiro.tg@gmail.com>
2017-10-01 10:07:15 -0300

    index

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
