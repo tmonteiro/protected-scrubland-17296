c004d0ae79d2d38866891ce9be20eb46798f442e
Tiago Monteiro <monteiro.tg@gmail.com>
2017-10-02 22:43:51 -0300

    teste

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
