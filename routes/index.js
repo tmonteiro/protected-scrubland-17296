eb2e3fe17302126dd3ecad36333f1ffd4c08b309
Tiago Monteiro <monteiro.tg@gmail.com>
2017-10-02 22:25:56 -0300

    teste

--- routes/index.js
+++ routes/index.js
@@ -93,7 +93,7 @@ var json_data = [
 // Set up Postgres using massive.js
 const massive = require('massive');
 var connectionString = process.env.DATABASE_URL + "?ssl=true" || "postgres://postgres@localhost/postgres";
-var massiveInstance = massive.connectSync({db : connectionString});
+var massiveInstance = massive.connectSync({connectionString : connectionString});
 // app.set('db', massiveInstance);
 
 router.get('/api/v1/post/teste', (req, res, next) => {
