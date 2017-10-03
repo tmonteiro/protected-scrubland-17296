3c5fffeadec24882825854f3e7072c713beb34c9
Tiago Monteiro <monteiro.tg@gmail.com>
2017-10-02 21:54:48 -0300

    teste

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json('error', {
     message: err.message,
     error: {}
   });
